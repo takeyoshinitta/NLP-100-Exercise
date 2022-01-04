5671b5dd081e5798ecdedb03393e08bae3c3eafc
Takeyoshi Nitta <c6afc0c0b9d6bc@gmail.com>
2022-01-03 22:58:01 -1000

    Update Exercise01.py

--- Exercise01.py
+++ Exercise01.py
@@ -1,9 +1,8 @@
-#01 Reversed String
-#Obtain the string that arranges letters of the string "stressed" in reverse order (tail to head) 
+# 01 Reversed String
+# Obtain the string that arranges letters of the string "stressed" in reverse order (tail to head) 
 str = 'stressed'
 strList = list(str)
-newStrList = []
+newStr = ''
 for i in range(len(strList), 0, -1):
-    newStrList.append(strList[i-1])
-newStr = ''.join(newStrList)
+    newStr += strList[i-1]
 print(newStr)
