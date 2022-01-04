a1530777142cec3ebcc425ad86a1858fd5d8556f
Takeyoshi Nitta <c6afc0c0b9d6bc@gmail.com>
2022-01-03 22:56:11 -1000

    Add files via upload

---
+++ Exercise02.py
@@ -0,0 +1,8 @@
+# 01 "schooled"
+# Obtain the string that concatenates the 1st, 3rd, 5th, and 7th letters in the string "schooled".
+str = 'schooled'
+strList = list(str)
+newStr = ''
+for i in range(1, len(strList), 2):
+    newStr += strList[i]
+print(newStr)
