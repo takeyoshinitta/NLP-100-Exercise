c1e7eb3ddc320765f31725d3524f760d5d45f230
Takeyoshi Nitta <c6afc0c0b9d6bc@gmail.com>
2022-01-04 08:45:38 -1000

    Add files via upload

---
+++ Exercise03.py
@@ -0,0 +1,10 @@
+# 03. "shoe" + "cold" = "schooled"
+# Obtain the string "schooled" by concatenating the letters in "shoe" and "cold" one after the other from head to tail.
+s1 = 'shoe'
+s2 = 'cold'
+s1List = list(s1)
+s2List = list(s2)
+newStr = ''
+for i in range(0, 4):
+    newStr += s1List[i] + s2List[i]
+print(newStr)
