abe967fd7c5086cb2844d1c8df34f31833fe010c
Takeyoshi Nitta <c6afc0c0b9d6bc@gmail.com>
2022-01-04 08:46:06 -1000

    Update and rename Exercise01.py to Exercise001.py

--- Exercise001.py
+++ Exercise001.py
@@ -1,4 +1,4 @@
-# 01 Reversed String
+# 01. Reversed String
 # Obtain the string that arranges letters of the string "stressed" in reverse order (tail to head) 
 str = 'stressed'
 strList = list(str)
