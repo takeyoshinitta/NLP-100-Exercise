6a98a8150507aa257d23f0bc21285a2170848049
Takeyoshi Nitta <c6afc0c0b9d6bc@gmail.com>
2022-01-13 16:07:23 -1000

    Update and rename Exercise009.py to Exercise08.py

--- Exercise08.py
+++ Exercise08.py
@@ -1,4 +1,4 @@
-# 009. cipher text
+# 08. cipher text
 """
 Implement a function cipher that converts a given string with the specification:
     *Every alphabetical lowercase letter c is converted to a letter whose ASCII code is (219 - [the ASCII code of c])
