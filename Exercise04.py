307f9dca1e6a31e0e859c3d91c2b0009105bcfdd
Takeyoshi Nitta <c6afc0c0b9d6bc@gmail.com>
2022-01-13 16:06:18 -1000

    Update and rename Exercise005.py to Exercise04.py

--- Exercise04.py
+++ Exercise04.py
@@ -1,4 +1,4 @@
-# 005. Atomic symbols
+# 04. Atomic symbols
 #Split the sentence "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can". into words, and extract the first letter from the 1st, 5th, 6th, 7th, 8th, 9th, 15th, 16th, 19th words and the first two letters from the other words. Create an associative array (dictionary object or mapping object) that maps from the extracted string to the position (offset in the sentence) of the corresponding word.
 
 s = 'Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can'
