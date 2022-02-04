aabe230d6949dc7fd262d04d1a2a6ab46ed13749
Takeyoshi Nitta <c6afc0c0b9d6bc@gmail.com>
2022-02-03 16:10:03 -1000

    Add files via upload

---
+++ 2_UNIXCommands/Exercise14.py
@@ -0,0 +1,11 @@
+# 14. First N lines
+# Receive a natural number $N$ from a command-line argument, and output the first $N$ lines of the file. Confirm the result by using head command.
+from sys import argv
+
+n = 0;
+if (argv[1].isdigit()):
+    n = argv[1]
+    with open('popular-names.txt') as f:
+        line = f.readlines()
+        for i in range(0, int(n)):
+            print(line[i].rstrip())
