a3aad85b7e64db5b92d9ba49514f9a6000af480f
Takeyoshi Nitta <c6afc0c0b9d6bc@gmail.com>
2022-02-03 16:20:44 -1000

    Add files via upload

---
+++ 2_UNIXCommands/Exercise15.py
@@ -0,0 +1,11 @@
+# 15. Last N lines
+# Receive a natural number $N$ from a command-line argument, and output the last $N$ lines of the file. Confirm the result by using tail command
+from sys import argv
+
+n = 0;
+if (argv[1].isdigit()):
+    n = int(argv[1])
+    with open('popular-names.txt') as f:
+        line = f.readlines()
+        for i in range(len(line)-1, len(line)-n-1, -1):
+            print(line[i].rstrip())
