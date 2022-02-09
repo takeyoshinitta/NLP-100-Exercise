84fdd657bb511c270464d4035f448167c86f6efb
Takeyoshi Nitta <c6afc0c0b9d6bc@gmail.com>
2022-02-09 13:58:56 -1000

    Add files via upload

---
+++ Exercise20.py
@@ -0,0 +1,12 @@
+# 20. Read JSON documents
+# Read the JSON documents and output the body of the article about the United Kingdom. Reuse the output in problems 21-29.
+
+import gzip
+import json
+
+with gzip.open('enwiki-country.json.gz') as data_file:
+    with open('output-exercise20-en.txt', 'w') as f:
+        for i in data_file:
+            data_json = json.loads(i)
+            if data_json['title'] == 'United Kingdom':
+                print(data_json['text'])
