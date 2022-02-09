1e39b7a448f0576b66ddf969a1b4b9d91fa6ce09
Takeyoshi Nitta <c6afc0c0b9d6bc@gmail.com>
2022-02-09 12:30:27 -1000

    Add files via upload

--- 2_UNIXCommands/Exercise17.py
+++ 2_UNIXCommands/Exercise17.py
@@ -1,7 +1,7 @@
 # 17. Distinct strings in the first column
 # Find distinct strings (a set of strings) of the first column of the file. Confirm the result by using cut, sort, and uniq commands.
 
-def removeDeplicate(list):
+def removeDuplicates(list):
     newList = []
     for i in list:
         if not(i in newList):
@@ -14,6 +14,6 @@ with open('popular-names.txt') as f:
     for i in lines:
         lineArray = i.split('\t')
         firstColumn.append(lineArray[0])
-    size = len(removeDeplicate(firstColumn))
+    size = len(removeDuplicates(firstColumn))
     print(f'There are {size} set of strings')
     f.close
