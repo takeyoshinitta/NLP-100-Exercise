8f2462bc26f9acea42a5d335d606909021bb1ce9
Takeyoshi Nitta <c6afc0c0b9d6bc@gmail.com>
2022-02-11 12:34:36 -1000

    Add files via upload

---
+++ 3_RegularExpression/Exercise22.py
@@ -0,0 +1,19 @@
+# 22. Category names
+# Extract the category names of the article.
+
+import gzip
+import json
+import re
+
+with gzip.open('enwiki-country.json.gz') as data_file:
+    for i in data_file:
+        data_json = json.loads(i)
+        if data_json['title'] == 'United Kingdom':
+            str = data_json['text']
+    lines = str.split('\n')
+
+    for i in lines:
+        if re.match(r'^\[\[Category:.+\]\]$', i):
+            # print (i[11:-2])
+            print(i.replace('[', '').replace(']', '').replace(':', '').replace('Category', '').replace('|', ''))
+    
