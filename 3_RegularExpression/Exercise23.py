8c40a9f99db26450b610dde6b42f1e94945b6763
Takeyoshi Nitta <c6afc0c0b9d6bc@gmail.com>
2022-02-14 14:36:55 -1000

    Add files via upload

---
+++ 3_RegularExpression/Exercise23.py
@@ -0,0 +1,22 @@
+# 23. Section structure
+# Extract section names in the article with their levels. For example, the level of the section is 1 for the MediaWiki markup "== Section name ==".
+
+import gzip
+import json
+import re
+
+with gzip.open('enwiki-country.json.gz') as data_file:
+    for i in data_file:
+        data_json = json.loads(i)
+        if data_json['title'] == 'United Kingdom':
+            str = data_json['text']
+    lines = str.split('\n')
+
+    for i in lines:
+        if re.match(r'^=+.+=+$', i):
+            s = re.match(r'^(=+)(.+?)=+$', i)
+            print (f'name: {format(s.group(2))}')
+            print (f'level: {len(s.group(1)) - 1}')
+            print ('\n')
+
+    data_file.close()
