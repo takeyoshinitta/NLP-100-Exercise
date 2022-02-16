e921f6d05117abbb0c8fa7216609e98dc88c6402
Takeyoshi Nitta <c6afc0c0b9d6bc@gmail.com>
2022-02-16 13:39:19 -1000

    Update Exercise24.py

--- 3_RegularExpression/Exercise24.py
+++ 3_RegularExpression/Exercise24.py
@@ -1,1 +1,15 @@
+# 24. Media references
+# Extract references to media files linked from the article.
 
+import gzip
+import json
+import re
+
+with gzip.open('enwiki-country.json.gz') as data_file:
+    for i in data_file:
+        data_json = json.loads(i)
+        if data_json['title'] == 'United Kingdom':
+            str = data_json['text']
+    lines = str.split('\n')
+
+    
